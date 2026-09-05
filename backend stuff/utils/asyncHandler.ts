66e8a07c106eb17c4bbca48270140924b9ff807e
keshav saini <sainikeshav241@gmail.com>
2026-09-05 21:35:46 +0530

    feat: add asyncHandler utility for handling asynchronous request handlers

---
+++ backend stuff/utils/asyncHandler.ts
@@ -0,0 +1,17 @@
+import type { Request, Response, NextFunction, RequestHandler } from "express";
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<unknown> | unknown;
+
+const asyncHandler = (requestHandler: AsyncRequestHandler): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+      next(error);
+    });
+  };
+};
+
+export default asyncHandler;
